58a08d0c00bd4d447d0f671ed71e3bbca4b5f1f6
Jonathan Ta <jdta@umich.edu>
2025-03-25 19:33:22 -0400

    added phi_w test

--- sample_code/sample_data_phi_test.R
+++ sample_code/sample_data_phi_test.R
@@ -7,20 +7,14 @@ set.seed(123)
 
 calc_C_phi = function(coords, phi) {
   n = nrow(coords)
-  C_phi = matrix(0, nrow = n, ncol = n)
-  for (i in 1:n) {
-    for (j in 1:n) {
-      # Calculate the distance between points i and j
-      C_phi[i, j] = exp(-0.5 * t(coords[i,] - coords[j,]) %*% (coords[i,] - coords[j,]) / phi)
-    }
-  }
-  return(C_phi)
+  dists = as.matrix(dist(coords)) ^ 2
+  C_phi = exp(-0.5 / phi * dists)
 }
 
 set.seed(123)
 
-lat = seq(0, 19, by = 1)
-lon = seq(0, 19, by = 1)
+lat = seq(0, 30, by = 1)
+lon = seq(0, 30, by = 1)
 
 coords = as.matrix(expand.grid(lat, lon))
 colnames(coords) = c("lat", "lon")
@@ -30,7 +24,7 @@ p = 2
 
 # generating w
 sigmasq_w = 1
-phi_w = 2
+phi_w = 8
 C_w = calc_C_phi(coords, phi_w)
 w = MASS::mvrnorm(1, rep(0, n), sigmasq_w * C_w)
 
@@ -96,7 +90,7 @@ grid.arrange(Y_plot, w_plot, beta_1_plot, beta_2_plot, epsilon_plot, ncol = 3)
 
 # generate knots
 ## 1 in every k^2 point on a grid is a knot
-k = 2
+k = 5
 
 lat_knots = unique(lat)
 lat_knots = lat_knots[seq(1, length(lat_knots), by = k)]
@@ -119,7 +113,7 @@ X_star = as.matrix(knots_df[, c("X_1", "X_2")])
 
 m = nrow(knots)
 l = 0
-u = 4
+u = 20
 
 # Test function
 
@@ -141,94 +135,30 @@ output = svc::GP_Gibbs_phi_test(
   sigmasq_w_start = sigmasq_w,
   tausq_start = tausq,
   phi_beta_proposal_sd = rep(0.1, p),
-  phi_w_proposal_sd = 0.1,
+  phi_w_proposal_sd = 0.2,
   lower_beta = rep(l, p),
   upper_beta = rep(u, p),
   lower_w = l,
   upper_w = u,
-  mcmc = 1000
+  mcmc = 5000
 )
 
-start = 100
-end = 1000
+start = 3000
+end = 5000
 
 # trace plot of phi_w
 plot(y = output$phi_w_samples[start:end], x = start:end, type = "l", main = "Trace plot of phi_w", ylab = "phi_w", xlab = "Iteration")
 mean(output$phi_w_samples[start:end])
+quantile(output$phi_w_samples[start:end], c(0.025, 0.975))
 phi_w
 
-# trace plot of phi_beta 1
-plot(y = output$phi_beta_samples[start:end, 1], x = start:end, type = "l", main = "Trace plot of phi_beta_1", ylab = "phi_beta_1", xlab = "Iteration")
-mean(output$phi_beta_samples[start:end, 1])
-phi_1
+# # trace plot of phi_beta 1
+# plot(y = output$phi_beta_samples[start:end, 1], x = start:end, type = "l", main = "Trace plot of phi_beta_1", ylab = "phi_beta_1", xlab = "Iteration")
+# mean(output$phi_beta_samples[start:end, 1])
+# phi_1
+# 
+# # trace plot of phi_beta 2
+# plot(y = output$phi_beta_samples[start:end, 2], x = start:end, type = "l", main = "Trace plot of phi_beta_2", ylab = "phi_beta_2", xlab = "Iteration")
+# mean(output$phi_beta_samples[start:end, 2])
+# phi_2
 
-# trace plot of phi_beta 2
-plot(y = output$phi_beta_samples[start:end, 2], x = start:end, type = "l", main = "Trace plot of phi_beta_2", ylab = "phi_beta_2", xlab = "Iteration")
-mean(output$phi_beta_samples[start:end, 2])
-phi_2
-
-
-output = svc::GP_Gibbs(
-  Y = Y,
-  X = X,
-  s = coords,
-  knots = knots,
-  beta_knots_start = matrix(0, nrow = m, ncol = p),
-  w_knots_start = rep(0, m),
-  phi_beta_start = rep(l + (u - l) / 2, p),
-  phi_w_start = l + (u - l) / 2,
-  sigmasq_beta_start = rep(1, p),
-  sigmasq_w_start = 1,
-  tausq_start = 1,
-  phi_beta_proposal_sd = rep(0.1, p),
-  phi_w_proposal_sd = 0.1,
-  a_beta = rep(2, p),
-  b_beta = rep(1, p),
-  a_w = 2,
-  b_w = 1,
-  a_t = 2,
-  b_t = 1e-4,
-  lower_beta = rep(l, p),
-  upper_beta = rep(u, p),
-  lower_w = l,
-  upper_w = u,
-  mcmc = 1000
-)
-
-dim(output$sigmasq_beta_samples)
-output$
-  
-  # trace plot of sigmasq_w
-  plot(output$sigmasq_w_samples, type = "l", main = "Trace plot of sigmasq_w", ylab = "sigmasq_w", xlab = "Iteration")
-mean(output$sigmasq_w_samples[500:1000])
-sigmasq_w
-
-# trace plot of phi_w
-plot(output$phi_w_samples, type = "l", main = "Trace plot of phi_w", ylab = "phi_w", xlab = "Iteration")
-mean(output$phi_w_samples[500:1000])
-phi_w
-
-# trace plot of tausq
-plot(output$tausq_samples, type = "l", main = "Trace plot of tausq", ylab = "tausq", xlab = "Iteration")
-mean(output$tausq_samples[500:1000])
-tausq
-
-# trace plot of sigmasq_beta 1
-plot(output$sigmasq_beta_samples[, 1], type = "l", main = "Trace plot of sigmasq_beta_1", ylab = "sigmasq_beta_1", xlab = "Iteration")
-mean(output$sigmasq_beta_samples[500:1000, 1])
-sigmasq_1
-
-# trace plot of sigmasq_beta 2
-plot(output$sigmasq_beta_samples[, 2], type = "l", main = "Trace plot of sigmasq_beta_2", ylab = "sigmasq_beta_2", xlab = "Iteration")
-mean(output$sigmasq_beta_samples[500:1000, 2])
-sigmasq_2
-
-# trace plot of phi_beta 1
-plot(output$phi_beta_samples[, 1], type = "l", main = "Trace plot of phi_beta_1", ylab = "phi_beta_1", xlab = "Iteration")
-mean(output$phi_beta_samples[500:1000, 1])
-phi_1
-
-# trace plot of phi_beta 2
-plot(output$phi_beta_samples[, 2], type = "l", main = "Trace plot of phi_beta_2", ylab = "phi_beta_2", xlab = "Iteration")
-mean(output$phi_beta_samples[500:1000, 2])
-phi_2
