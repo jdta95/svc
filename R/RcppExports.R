eb0eb9a9941228d1648572b0dd8e09deef693975
Justice Akuoko-Frimpong <jakuokof@umich.edu>
2025-04-03 09:25:13 -0400

    small changes

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -9,3 +9,7 @@ GP_Gibbs_phi_test <- function(Y, X, s, knots, beta_knots_start, w_knots_start, p
     .Call(`_svc_GP_Gibbs_phi_test`, Y, X, s, knots, beta_knots_start, w_knots_start, phi_beta_start, phi_w_start, sigmasq_beta_start, sigmasq_w_start, tausq_start, phi_beta_proposal_sd, phi_w_proposal_sd, lower_beta, upper_beta, lower_w, upper_w, mcmc)
 }
 
+GP_Gibbs_test_tau_sigma <- function(Y, X, s, knots, beta_knots_start, w_knots_start, phi_beta_start, phi_w_start, sigmasq_beta_start, sigmasq_w_start, tausq_start, phi_beta_proposal_sd, phi_w_proposal_sd, a_beta, b_beta, a_w, b_w, a_t, b_t, lower_beta, upper_beta, lower_w, upper_w, mcmc = 1000L) {
+    .Call(`_svc_GP_Gibbs_test_tau_sigma`, Y, X, s, knots, beta_knots_start, w_knots_start, phi_beta_start, phi_w_start, sigmasq_beta_start, sigmasq_w_start, tausq_start, phi_beta_proposal_sd, phi_w_proposal_sd, a_beta, b_beta, a_w, b_w, a_t, b_t, lower_beta, upper_beta, lower_w, upper_w, mcmc)
+}
+
